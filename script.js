1975d54194e0ec419a96a30eca499890bd1ebf7e
Philip Thomsen <philip-th@live.dk>
2023-04-17 12:37:53 +0200

    🔧 chore(script.js): update service worker registration to include error handling and logging 🔧 chore(sw.js): remove caching logic and add console logs to install, activate, and fetch events

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-  }
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(reg => console.log('service worker registered', reg))
+	.catch(err => console.error('service worker not registered', err)) 
+}
 
 //Grab day of the week from local computer
 let date = new Date();
